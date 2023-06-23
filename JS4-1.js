077b75e9317e3e59a07dcdf74507d817ebfbf34c
tash97 <tasha.nicole.smith.97@gmail.com>
2023-06-23 05:35:36 -0400

    first commit

--- JS4-1.js
+++ JS4-1.js
@@ -6,20 +6,47 @@
 
 // HW:
 //* 1.  Filter out all of the empty strings from the list below
-// Input: let places = [" ","Argentina", " ", "San Diego","","  ","","Boston","New York"]
+// Input: 
+let placess = [" ","Argentina", " ", "San Diego","","  ","","Boston","New York"]
 // Output: ['Argentina', 'San Diego', 'Boston', 'New York']
+
+let filter = placess.filter(place => place.length > 2);
+console.log('Question 1:\nThe output of filtering out the empty strings in the provided list is:');
+console.log(filter);
 
 
 //* 2. Write an anonymous function that sorts this list by the last name
-// Input: let author = ["Joel Carter", "Victor aNisimov", "Andrew P. Garfield","David hassELHOFF","Gary A.J. Bernstein"]
+// Input: 
+let author = ["Joel Carter", "Victor aNisimov", "Andrew P. Garfield","David hassELHOFF","Gary A.J. Bernstein"]
 // Output: ['Victor aNisimov', 'Gary A.J. Bernstein', 'Joel Carter', 'Andrew P. Garfield', 'David hassELHOFF']
 
+let map = author.map((list) => {
+    let splitter = list.split(" ");
+    let reverse = splitter.reverse();
+    return reverse;    
+})
+.sort((a,b) => {
+    return a[0].toLowerCase().localeCompare(b[0].toLowerCase());
+})
+.map((namePiece) => {
+    reverseReverse = namePiece.reverse();
+    joiner = reverseReverse.join(" ")
+    return joiner
+    
+});
+console.log('Question 2:\nThe provided list sorted by last name is as follows:')
+console.log(map);
 
 //* 3. Convert the list below from Celsius to Farhenheit, using the map function
 // # F = (9/5)*C + 32
-// places = [('Nashua',32),("Boston",12),("Los Angelos",44),("Miami",29)]
- 
+// 
+let places = [('Nashua',32),("Boston",12),("Los Angelos",44),("Miami",29)]
 // Output: [('Nashua', 89.6), ('Boston', 53.6), ('Los Angelos', 111.2), ('Miami', 84.2)]
+
+
+
+
+console.log('Question 3:\nI think question 3 might not be possible?\nWhen I grab an item from the places array it only shows the second item in each parenthesis\nUpon further research, I believe the comma in the parenthesis is working as a comma operator\nAnd is only allowing the last value in each array item to be returned\nThus making it impossible to grab the first item in each parenthesis');
 
 
 //* 4. Write a recursion function to perform the fibonacci sequence up to the number passed in.
@@ -32,3 +59,25 @@
 // Iteration 3: 3
 // Iteration 4: 5
 // Iteration 5: 8
+function fib(num){
+    let i = 0;
+    let num1 = 0;
+    let num2 = 1;
+    let sum;
+    function fibi(num, num1, num2, sum, i) {
+        if(i === 0){
+            console.log(`iteration ${i}: 1`);
+            i++;
+            fibi(num, num1, num2, sum, i);
+        }else if(i <= num){
+            sum = num1 + num2;
+            num1 = num2;
+            num2 = sum;
+            i++;
+            console.log(`Iteration ${i}: ${sum}`);
+            fibi(num, num1, num2, sum, i);
+        }
+    }
+    fibi(num, num1, num2, sum, i);
+}
+fib(5);
